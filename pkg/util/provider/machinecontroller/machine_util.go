6515f815e4abe616b14fdbfb0932b539ae2c0d52
sondx12 <sondx12@fpt.com.vn>
2023-06-16 18:28:20 -0700

    fix-bug-get-log

--- pkg/util/provider/machinecontroller/machine_util.go
+++ pkg/util/provider/machinecontroller/machine_util.go
@@ -641,33 +641,35 @@ func (c *controller) reconcileMachineHealth(machine *v1alpha1.Machine) (machineu
 							}
 						}
 						i := 0
-						for ; i < 5; i++ {
-							podLog := clientset.CoreV1().Pods("gpu-operator").GetLogs(Pod.Name,
-								&v1.PodLogOptions{Container: "nvidia-operator-validator"})
-							log, err := podLog.Stream()
-							if err != nil {
-								klog.Warning(err)
-							}
-							defer log.Close()
-							buf := new(bytes.Buffer)
-							_, err = io.Copy(buf, log)
-							if err != nil {
-								klog.Warning(err)
-							}
-							logOutput := buf.String()
-							if strings.Contains(logOutput, "all validations are successful") {
-								var updatedTaints []corev1.Taint
-								for _, taint := range node.Spec.Taints {
-									if taint.Key != "nvidia.com/gpu" {
-										updatedTaints = append(updatedTaints, taint)
-									}
-								}
-								node.Spec.Taints = updatedTaints
-								_, err = c.targetCoreClient.CoreV1().Nodes().Update(node)
+						for ; i < 6; i++ {
+							if Pod.Status.Phase == v1.PodRunning {
+								podLog := clientset.CoreV1().Pods("gpu-operator").GetLogs(Pod.Name,
+									&v1.PodLogOptions{Container: "nvidia-operator-validator"})
+								log, err := podLog.Stream()
 								if err != nil {
 									klog.Warning(err)
 								}
-								break
+								defer log.Close()
+								buf := new(bytes.Buffer)
+								_, err = io.Copy(buf, log)
+								if err != nil {
+									klog.Warning(err)
+								}
+								logOutput := buf.String()
+								if strings.Contains(logOutput, "all validations are successful") {
+									var updatedTaints []corev1.Taint
+									for _, taint := range node.Spec.Taints {
+										if taint.Key != "nvidia.com/gpu" {
+											updatedTaints = append(updatedTaints, taint)
+										}
+									}
+									node.Spec.Taints = updatedTaints
+									_, err = c.targetCoreClient.CoreV1().Nodes().Update(node)
+									if err != nil {
+										klog.Warning(err)
+									}
+									break
+								}
 							} else {
 								time.Sleep(1 * time.Minute)
 							}
