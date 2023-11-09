d41de86b1482339fdd382c3f03336f4c5e37b0a6
Dao Xuan Son <sondx12@fpt.com>
2023-11-09 00:22:55 -0800

    update-controller-check-GPU

--- pkg/util/provider/machinecontroller/node.go
+++ pkg/util/provider/machinecontroller/node.go
@@ -18,9 +18,16 @@ limitations under the License.
 package controller
 
 import (
+	"bytes"
+	"context"
+	"io"
+	"strings"
+	"time"
+
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/klog/v2"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/cache"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -49,6 +56,7 @@ func (c *controller) nodeDelete(obj interface{}) {
 
 // Not being used at the moment, saving it for a future use case.
 func (c *controller) reconcileClusterNodeKey(key string) error {
+	ctx := context.Background()
 	node, err := c.nodeLister.Get(key)
 	if apierrors.IsNotFound(err) {
 		return nil
@@ -57,10 +65,68 @@ func (c *controller) reconcileClusterNodeKey(key string) error {
 		klog.Errorf("ClusterNode %q: Unable to retrieve object from store: %v", key, err)
 		return err
 	}
-
-	return c.reconcileClusterNode(node)
+	klog.V(4).Infof("Reconcile Node [%s]", node.Name)
+	err = c.reconcileClusterNode(ctx, node)
+	if err != nil {
+		// Re-enqueue after a 30s window
+		c.enqueueNodeAfter(node, 30*time.Second)
+	} else {
+		// Re-enqueue periodically to avoid missing of events
+		// TODO: Get ride of this logic
+		c.enqueueNodeAfter(node, 3*time.Minute)
+	}
+	return nil
 }
 
-func (c *controller) reconcileClusterNode(node *v1.Node) error {
+func (c *controller) reconcileClusterNode(ctx context.Context, node *v1.Node) error {
+	if node.Labels["worker.fptcloud/type"] == "gpu" {
+		pods, err := c.targetCoreClient.CoreV1().Pods("fptcloud-gpu-operator").List(ctx, metav1.ListOptions{LabelSelector: "app=nvidia-mig-manager"})
+		if err != nil {
+			return err
+		}
+		for _, p := range pods.Items {
+			if p.Spec.NodeName == node.Name && p.Status.Phase == v1.PodRunning {
+				klog.V(4).Infof("pod [%s] in node [%s]", p.Name, node.Name)
+				podLog := c.targetCoreClient.CoreV1().Pods("fptcloud-gpu-operator").GetLogs(p.Name,
+					&v1.PodLogOptions{Container: "nvidia-mig-manager"})
+				log, err := podLog.Stream(ctx)
+				if err != nil {
+					klog.Warning(err)
+				}
+				defer log.Close()
+				buf := new(bytes.Buffer)
+				_, err = io.Copy(buf, log)
+				if err != nil {
+					klog.Warning(err)
+				}
+				logOutput := buf.String()
+				klog.V(4).Infof("Log of Pod %s: %s", p.Name, logOutput)
+				if strings.Contains(logOutput, "error setting MIGConfig: error attempting multiple config orderings: all orderings failed") {
+					label := make(map[string]string)
+					for k, v := range node.Labels {
+						if !strings.Contains(k, "nvidia.com/mig.config") {
+							label[k] = v
+						}
+					}
+					// nodeP, _ := c.targetCoreClient.CoreV1().Nodes().Get(ctx, n.Name, metav1.GetOptions{})
+					klog.V(4).Infof("Updating label for node %s", node.Name)
+					node.Labels = label
+					_, err := c.targetCoreClient.CoreV1().Nodes().Update(ctx, node, metav1.UpdateOptions{})
+					if err != nil {
+						klog.V(4).Infof("Error when updating node %s: %v", node.Name, err.Error())
+						return err
+					}
+					c.targetCoreClient.CoreV1().Pods("fptcloud-gpu-operator").Delete(ctx, p.Name, metav1.DeleteOptions{})
+				}
+
+			}
+		}
+	}
 	return nil
+}
+func (c *controller) enqueueNodeAfter(obj interface{}, after time.Duration) {
+	if toBeEnqueued, key := c.isToBeEnqueued(obj); toBeEnqueued {
+		klog.V(5).Infof("Adding node object to the queue %q after %s", key, after)
+		c.machineQueue.AddAfter(key, after)
+	}
 }
